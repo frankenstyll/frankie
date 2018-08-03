567d0c3d1bc7963c30cfb3bf041e5c80671d1107
kampon <kampon@NR-0000041.locus.co.th>
2018-08-03 10:39:37 +0700

    update code

---
+++ 1stspringbootandangular/src/main/java/com/web/controller/test/TestController.java
@@ -0,0 +1,18 @@
+package com.web.controller.test;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TestController {
+
+	@RequestMapping(value = { "test/index" })
+	public ModelAndView index(HttpServletRequest req, Model model) {
+		return new ModelAndView("index");
+	}
+
+}
