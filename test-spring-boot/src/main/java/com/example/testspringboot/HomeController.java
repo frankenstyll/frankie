21cd102d1e8a1b712932f0e741fb9d6870f2a98f
kampon <kampon@NR-0000041.locus.co.th>
2019-01-11 18:02:59 +0700

    commit

--- test-spring-boot/src/main/java/com/example/testspringboot/HomeController.java
+++ test-spring-boot/src/main/java/com/example/testspringboot/HomeController.java
@@ -1,16 +1,24 @@
 package com.example.testspringboot;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping(value = { "/", "/home" })
-	public String index(Map<String, Object> model) {
+	public String home( Model model) {
 		System.out.println("*****************HOME******************");
-		return "home";
-	}
+        return "home";
+    }
+
+	@GetMapping(value = { "/login"})
+	public String login( Model model) {
+		System.out.println("*****************HOME******************");
+        model.addAttribute("name", "NAMEEE");
+        return "login/login";
+    }
+
 }
